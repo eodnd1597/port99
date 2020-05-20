d05f293241f8cf0247d7f6274f1aaf52164d9116
eodnd1597 <65323661+eodnd1597@users.noreply.github.com>
2020-05-20 19:20:26 +0900

    2020.05.20

---
+++ README.md
@@ -0,0 +1,4 @@
+
+<h1>포트폴리오 사이트</h1>
+
+<a href="https://eodnd1597.github.io/port/index.html" target="_blank">메인</a>
---
+++ nongsim/assets/js/custom.js
@@ -0,0 +1,30 @@
+var mySwiper = new Swiper ('#banner .swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: 'true',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 5000,
+    }
+})
+
+var mySwiper2 = new Swiper ('.notice .swiper-container', {
+  loop: true,
+  slidesPerView: 3,
+      spaceBetween: 30,
+      loop: true,
+    spaceBetween: 60,
+    slidesPerView: 'auto',
+    centeredSlides: true
+})
+var mySwiper3 = new Swiper ('.story .swiper-container', {
+  loop: true,
+  slidesPerView: '6',
+  spaceBetween: '60',
+  centeredSlides: true
+})
