47717f2436a726dad2dc4deaa455004d7e50fa41
eodnd1597 <65323661+eodnd1597@users.noreply.github.com>
2020-07-02 20:09:09 +0900

    07.02

--- port/assets/js/site1.js
+++ port/assets/js/site1.js
@@ -1,9 +1,37 @@
-$("#section-wrap > #section > figure").click(function(){
+$(".site-menu > li > a").click(function(){
+    $(".site-menu > li > a").removeClass("active");
+    $(this).addClass("active");
+});
+//메인 탭메뉴
+
+$("#section-wrap > #section").click(function(){
     let target = $(this);
     let index = target.index();
-    //alert(index)
-    $(".layer-site").eq(index).addClass("show");
+    // alert(index)
+    $(".site-wrap > .layer-site ").removeClass("show")
+    $(".site-wrap > .layer-site ").eq(index).addClass("show");
 });
+
 $(".close").click(function(){
     $(".layer-site").removeClass("show");
 });
+//레이어 팝업
+
+
+$(".list").click(function(){
+    let target = $(this);
+    let index = target.index();
+    $(".btn").removeClass("active");
+    $(".btn").eq(index).addClass("active");
+    $(".btn-cont").removeClass("show")
+});
+
+$(".last-btn").click(function(){
+    let target = $(this);
+    let index = target.index();
+    $(".btn-cont").removeClass("show");
+    $(".btn-cont").eq(index).addClass("show");
+    $(".last-btn").removeClass("active");
+    $(".last-btn").eq(index).addClass("active");
+});
+
