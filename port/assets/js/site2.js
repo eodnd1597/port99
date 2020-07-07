2f1ed4ddbfc9ad1ea8723f61cc4e364041b0f2c8
eodnd1597 <65323661+eodnd1597@users.noreply.github.com>
2020-07-07 09:59:53 +0900

    07.07

--- port/assets/js/site2.js
+++ port/assets/js/site2.js
@@ -4,6 +4,8 @@ $(".site-menu > li > a").click(function(){
 });
 //메인 탭메뉴
 
+
+
 $("#section-wrap > #section").click(function(){
     let target = $(this);
     let index = target.index();
@@ -18,18 +20,35 @@ $(".close").click(function(){
 //레이어 팝업
 
 
+let index1;
 $(".list").click(function(){
     let target = $(this);
-    let index = target.index();
+    index1 = target.index();
     $(".code-tit").removeClass("active");
-    $(".code-tit").eq(index).addClass("active");
+    $(".code-tit").eq(index1).addClass("active");
     //제목 변경
-    $(".btn-wrap").removeClass("active");
-    $(".btn-wrap").eq(index).addClass("active");
-    console.log(index)
+    $(".btn-wrap").removeClass("show");
+    $(".btn-wrap").eq(index1).addClass("show");
+    //전체 틀 변경
+    $(".list").removeClass("active");
+    $(".list").eq(index1 -1).addClass("active");
+    console.log(index1)
 });
 
-
+$(".last-btn").click(function(){
+    let target = $(this)
+    let index = target.index();
+    
+    //console.log($(".a").eq(index1 -1).find(".btn-cont").eq(index));
+    $(".btn-cont").removeClass("show");
+    $(".a").eq(index1 -1).find(".btn-cont").eq(index).addClass("show");
+    
+    // $(".btn-cont").removeClass("show");
+    // $(".btn-cont").eq(index).addClass("show");
+   
+    //html,css,js 변경
+    
+});
 
 
 
